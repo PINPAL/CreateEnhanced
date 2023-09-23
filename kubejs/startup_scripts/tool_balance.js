e845918600c000aacedf6ea8ee4a0eb8e12f7020
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-09-23 18:22:58 +0100

    Fix Syntax Errors

--- kubejs/startup_scripts/tool_balance.js
+++ kubejs/startup_scripts/tool_balance.js
@@ -1,46 +1,46 @@
 ItemEvents.modification((event) => {
   event.modify("minecraft:golden_sword", (item) => {
-    maxDamage = 180;
+    item.maxDamage = 180;
   });
   event.modify("minecraft:golden_hoe", (item) => {
-    maxDamage = 180;
-    digSpeed = 6;
+    item.maxDamage = 180;
+    item.digSpeed = 6;
   });
   event.modify("minecraft:golden_chestplate", (item) => {
-    maxDamage = 220;
+    item.maxDamage = 220;
   });
   event.modify("minecraft:golden_boots", (item) => {
-    maxDamage = 190;
+    item.maxDamage = 190;
   });
   event.modify("minecraft:golden_helmet", (item) => {
-    maxDamage = 150;
+    item.maxDamage = 150;
   });
   event.modify("minecraft:golden_leggings", (item) => {
-    maxDamage = 200;
+    item.maxDamage = 200;
   });
 
   // Paxel
   event.modify("easypaxellite:wood_paxel", (item) => {
-    attackDamage = 2;
+    item.attackDamage = 2;
   });
   event.modify("easypaxellite:stone_paxel", (item) => {
-    attackDamage = 3;
+    item.attackDamage = 3;
   });
   event.modify("easypaxellite:golden_paxel", (item) => {
-    maxDamage = 500;
-    digSpeed = 6;
-    attackDamage = 4;
+    item.maxDamage = 500;
+    item.digSpeed = 6;
+    item.attackDamage = 4;
   });
   event.modify("easypaxellite:iron_paxel", (item) => {
-    attackDamage = 5;
+    item.attackDamage = 5;
   });
   event.modify("easypaxellite:diamond_paxel", (item) => {
-    attackDamage = 6;
+    item.attackDamage = 6;
   });
   event.modify("easypaxellite:netherite_paxel", (item) => {
-    attackDamage = 7;
+    item.attackDamage = 7;
   });
   event.modify("easypaxellite:tempered_netherite_paxel", (item) => {
-    attackDamage = 8;
+    item.attackDamage = 8;
   });
 });
