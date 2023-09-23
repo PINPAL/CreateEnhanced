b440fbdf7d5309a84f1b2a1843d3356a1dd9804f
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-09-23 18:27:33 +0100

    Temp fix for paxel attack dmg

--- kubejs/startup_scripts/tool_balance.js
+++ kubejs/startup_scripts/tool_balance.js
@@ -20,27 +20,8 @@ ItemEvents.modification((event) => {
   });
 
   // Paxel
-  event.modify("easypaxellite:wood_paxel", (item) => {
-    item.attackDamage = 2;
-  });
-  event.modify("easypaxellite:stone_paxel", (item) => {
-    item.attackDamage = 3;
-  });
   event.modify("easypaxellite:golden_paxel", (item) => {
     item.maxDamage = 500;
     item.digSpeed = 6;
-    item.attackDamage = 4;
-  });
-  event.modify("easypaxellite:iron_paxel", (item) => {
-    item.attackDamage = 5;
-  });
-  event.modify("easypaxellite:diamond_paxel", (item) => {
-    item.attackDamage = 6;
-  });
-  event.modify("easypaxellite:netherite_paxel", (item) => {
-    item.attackDamage = 7;
-  });
-  event.modify("easypaxellite:tempered_netherite_paxel", (item) => {
-    item.attackDamage = 8;
   });
 });
