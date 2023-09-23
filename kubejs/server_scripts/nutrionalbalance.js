6bc742a4c0b24d98ca8b9285b342ef4d8596003f
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-09-23 03:14:54 +0100

    Cooking Pot Fix

--- kubejs/server_scripts/nutrionalbalance.js
+++ kubejs/server_scripts/nutrionalbalance.js
@@ -1,3 +1,11 @@
 ServerEvents.recipes((event) => {
   event.remove({ output: "nutritionalbalance:lunchbox" });
+
+  // Cooking Pot
+  event.remove({ output: "farmersdelight:cooking_pot" });
+  event.shaped(Item.of("farmersdelight:cooking_pots"), ["B B", "IWI", "III"], {
+    B: "minecraft:brick",
+    W: "minecraft:water_bucket",
+    I: "#forge:ingots/iron",
+  });
 });
