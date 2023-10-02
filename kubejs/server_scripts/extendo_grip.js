31646f8c043e0a73f0d0b960cbdbdd6fff930cb9
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-10-02 06:32:50 +0100

    Fix Extendo Grip Curios

---
+++ kubejs/server_scripts/extendo_grip.js
@@ -0,0 +1,21 @@
+ServerEvents.recipes((event) => {
+  event.remove({ output: "create:extendo_grip" });
+  event.recipes.createMechanicalCrafting(
+    Item.of(
+      "create:extendo_grip",
+      '{CurioAttributeModifiers:[{AttributeName:"forge:reach_distance",Name:"forge:reach_distance",Amount:3,Operation:0,UUID:[I;775495772,434720553,-1780503515,1389346287],Slot:"hands"}]}'
+    ),
+    [" I ", " P ", "SSS", "SSS", " H "],
+    {
+      I: "#forge:ingots/brass",
+      P: "create:precision_mechanism",
+      S: "minecraft:stick",
+      H: "create:brass_hand",
+    }
+  );
+});
+
+// Tag items as curios
+ServerEvents.tags("item", (event) => {
+  event.add("curios:hands", "create:extendo_grip");
+});
