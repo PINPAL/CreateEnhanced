fd13d5b7137ee8c967573ec4227dc6bc4eb9d603
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-10-13 21:08:30 +0100

    EMI Cleanup

---
+++ kubejs/client_scripts/emi/hide_fluid.js
@@ -0,0 +1,28 @@
+const hideFluids = [
+  "sophisticatedcore:xp_still",
+  "createindustry:napalm",
+  "createindustry:kerosene",
+  "createindustry:naphta",
+  "cofh_core:experience",
+  "cofh_core:honey",
+  "cofh_core:potion",
+  "create_dd:condense_milk",
+  "create_dd:cream",
+  "create_dd:vanilla",
+  "create_dd:vanilla_milkshake",
+  "create_dd:strawberry",
+  "create_dd:strawberry_milkshake",
+  "create_dd:glowberry",
+  "create_dd:glowberry_milkshake",
+  "create_dd:caramel",
+  "create_dd:caramel_milkshake",
+  "create_dd:hot_chocolate",
+  "create_dd:chocolate_milkshake",
+];
+
+JEIEvents.hideFluids((event) => {
+  // Hide Fluids
+  hideFluids.forEach((fluid) => {
+    event.hide(fluid);
+  });
+});
