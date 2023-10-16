cf6a2987283fe13d31f98195170b0e1ed8bde91c
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-10-16 22:11:30 +0100

    Extendo Grip

--- kubejs/server_scripts/extendo_grip.js
+++ kubejs/server_scripts/extendo_grip.js
@@ -2,7 +2,7 @@ ServerEvents.recipes((event) => {
   event.recipes.createMechanicalCrafting(
     Item.of(
       "create:extendo_grip",
-      '{Unbreakable:1b,CurioAttributeModifiers:[{AttributeName:"forge:attack_range",Name:"forge:attack_range",Amount:3,Operation:0,UUID:[I;-1826537097,-1713487428,-1545263740,-720620157],Slot:"hands"},{AttributeName:"forge:reach_distance",Name:"forge:reach_distance",Amount:3,Operation:0,UUID:[I;775495772,434720553,-1780503515,1389346287],Slot:"hands"}]}'
+      '{Unbreakable:1b,CurioAttributeModifiers:[{AttributeName:"generic.attack_knockback",Name:"generic.attack_knockback",Amount:3,Operation:0,UUID:[I;1203229738,-1965539080,-1341673873,592198839],Slot:"hands"},{AttributeName:"forge:attack_range",Name:"forge:attack_range",Amount:3,Operation:0,UUID:[I;-1826537097,-1713487428,-1545263740,-720620157],Slot:"hands"},{AttributeName:"forge:reach_distance",Name:"forge:reach_distance",Amount:3,Operation:0,UUID:[I;775495772,434720553,-1780503515,1389346287],Slot:"hands"}]}'
     ),
     [" I ", " P ", "SSS", "SSS", " H "],
     {
