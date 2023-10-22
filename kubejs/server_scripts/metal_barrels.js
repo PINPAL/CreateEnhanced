9398ef36ad4e59b975e66b4ab71d1ee7f2d875e0
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-10-22 21:49:40 +0100

    Metal Barrels Items

--- kubejs/server_scripts/metal_barrels.js
+++ kubejs/server_scripts/metal_barrels.js
@@ -3,12 +3,26 @@ ServerEvents.recipes((event) => {
     "minecraft:barrel",
     "create:andesite_alloy",
   ]);
+  event.recipes.create.deploying("metalbarrels:wood_to_iron", [
+    "minecraft:stick",
+    "create:andesite_alloy",
+  ]);
+
   event.recipes.create.deploying("metalbarrels:gold_barrel", [
     "metalbarrels:iron_barrel",
     "#forge:ingots/copper",
   ]);
+  event.recipes.create.deploying("metalbarrels:iron_to_gold", [
+    "minecraft:stick",
+    "#forge:ingots/copper",
+  ]);
+
   event.recipes.create.deploying("metalbarrels:diamond_barrel", [
     "metalbarrels:gold_barrel",
+    "#forge:ingots/brass",
+  ]);
+  event.recipes.create.deploying("metalbarrels:gold_to_diamond", [
+    "minecraft:stick",
     "#forge:ingots/brass",
   ]);
 });
