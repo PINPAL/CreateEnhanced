bf845642ec84a0450ef1422c8b2979acd69bdbb2
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-10-27 22:34:02 +0100

    Prevent mobs dropping armor/tools

--- kubejs/server_scripts/loot.js
+++ kubejs/server_scripts/loot.js
@@ -9,3 +9,13 @@ LootJS.modifiers((event) => {
     event.addLootTypeModifier(LootType.CHEST).removeLoot(item);
   });
 });
+
+// Disable armor and tool drops from mobs
+EntityEvents.spawned((event) => {
+  const { entity } = event;
+  if (!entity.alive) return;
+  let nbt = entity.getNbt();
+  nbt.ArmorDropChances = [NBT.f(0), NBT.f(0), NBT.f(0), NBT.f(0)];
+  nbt.HandDropChances = [NBT.f(0), NBT.f(0)];
+  entity.setNbt(nbt);
+});
