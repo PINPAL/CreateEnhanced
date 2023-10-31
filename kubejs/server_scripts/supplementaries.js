e743c74913cbaaa586eed7b7ad24e9bcc2e99ea3
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-10-31 10:50:33 +0000

    Rotten flesh to ash

--- kubejs/server_scripts/supplementaries.js
+++ kubejs/server_scripts/supplementaries.js
@@ -1,15 +1,16 @@
 ServerEvents.recipes((event) => {
-  // Vertical Redstone (Cog Block)
-  event.shaped(
-    Item.of("supplementaries:cog_block", 8), // arg 1: output
-    [" C ", "BRB", " C "],
-    {
-      B: "createdeco:brass_bars",
-      C: "create:brass_casing",
-      R: "minecraft:redstone",
-    }
-  );
+	// Vertical Redstone (Cog Block)
+	event.shaped(
+		Item.of("supplementaries:cog_block", 8), // arg 1: output
+		[" C ", "BRB", " C "],
+		{
+			B: "createdeco:brass_bars",
+			C: "create:brass_casing",
+			R: "minecraft:redstone",
+		}
+	);
 
-  // Bones into Ash
-  event.smelting("3x supplementaries:ash", "minecraft:bone");
+	// Bones into Ash
+	event.smelting("3x supplementaries:ash", "minecraft:bone");
+	event.smelting("supplementaries:ash", "minecraft:rotten_flesh");
 });
