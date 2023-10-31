b28609bc6945c3b619cab97b27b32a5e23fae5ce
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-10-31 10:58:42 +0000

    Harder Fluid Tank

--- kubejs/server_scripts/_removals/outputs.js
+++ kubejs/server_scripts/_removals/outputs.js
@@ -1,159 +1,160 @@
 // priority: 1
 const outputsToRemove = [
-  // Tools & Armor
-  "#forge:pickaxes",
-  "#forge:shovels",
-  "#forge:axes",
-  "#forge:swords",
-  "#forge:hoes",
-  "#forge:tools/knives",
-  "#forge:armors/chestplates",
-  "#forge:armors/helmets",
-  "#forge:armors/leggings",
-  "#forge:armors/boots",
+	// Tools & Armor
+	"#forge:pickaxes",
+	"#forge:shovels",
+	"#forge:axes",
+	"#forge:swords",
+	"#forge:hoes",
+	"#forge:tools/knives",
+	"#forge:armors/chestplates",
+	"#forge:armors/helmets",
+	"#forge:armors/leggings",
+	"#forge:armors/boots",
 
-  // Minecraft
-  "minecraft:lodestone",
+	// Minecraft
+	"minecraft:lodestone",
 
-  // Vertical Slabs
-  /createdeco:.*slab_vert/,
-  /alloyed:.*vertical_slab/,
-  /v_slab_compat:createindustry\/.*concrete.*/,
+	// Vertical Slabs
+	/createdeco:.*slab_vert/,
+	/alloyed:.*vertical_slab/,
+	/v_slab_compat:createindustry\/.*concrete.*/,
 
-  // Supplementaries
-  "supplementaries:cog_block",
-  "supplementaries:bubble_blower",
-  "supplementaries:crank",
-  "supplementaries:faucet",
-  "supplementaries:bamboo_spikes",
-  "supplementaries:bamboo_spikes_tipped",
-  "supplementaries:bomb",
+	// Supplementaries
+	"supplementaries:cog_block",
+	"supplementaries:bubble_blower",
+	"supplementaries:crank",
+	"supplementaries:faucet",
+	"supplementaries:bamboo_spikes",
+	"supplementaries:bamboo_spikes_tipped",
+	"supplementaries:bomb",
 
-  // Nutritional Balance
-  "nutritionalbalance:lunchbox",
+	// Nutritional Balance
+	"nutritionalbalance:lunchbox",
 
-  // Farmers Delight
-  "farmersdelight:cooking_pot",
+	// Farmers Delight
+	"farmersdelight:cooking_pot",
 
-  // Alloyed
-  "alloyed:bronze_ingot",
-  "alloyed:bronze_nugget",
-  "alloyed:bronze_sheet",
-  "alloyed:bronze_block",
-  "alloyed:steel_ingot",
-  "alloyed:steel_nugget",
-  "alloyed:steel_sheet",
-  "alloyed:steel_pickaxe",
-  "alloyed:steel_shovel",
-  "alloyed:steel_axe",
-  "alloyed:steel_shears",
-  "alloyed:steel_fishing_rod",
-  "alloyed:steel_sword",
-  "alloyed:steel_hoe",
-  "alloyed:steel_helmet",
-  "alloyed:steel_chestplate",
-  "alloyed:steel_leggings",
-  "alloyed:steel_boots",
+	// Alloyed
+	"alloyed:bronze_ingot",
+	"alloyed:bronze_nugget",
+	"alloyed:bronze_sheet",
+	"alloyed:bronze_block",
+	"alloyed:steel_ingot",
+	"alloyed:steel_nugget",
+	"alloyed:steel_sheet",
+	"alloyed:steel_pickaxe",
+	"alloyed:steel_shovel",
+	"alloyed:steel_axe",
+	"alloyed:steel_shears",
+	"alloyed:steel_fishing_rod",
+	"alloyed:steel_sword",
+	"alloyed:steel_hoe",
+	"alloyed:steel_helmet",
+	"alloyed:steel_chestplate",
+	"alloyed:steel_leggings",
+	"alloyed:steel_boots",
 
-  // Create Deco
-  "createdeco:netherite_bars",
+	// Create Deco
+	"createdeco:netherite_bars",
 
-  // Explorers Compass
-  "explorerscompass:explorerscompass",
+	// Explorers Compass
+	"explorerscompass:explorerscompass",
 
-  // Create
-  "create:cart_assembler",
-  "create:rotation_speed_controller",
-  "create:hand_crank",
-  "create:spout",
-  "create:steam_engine",
-  "create:deployer",
-  "create:controls",
-  "create:mechanical_drill",
-  "create:extendo_grip",
+	// Create
+	"create:cart_assembler",
+	"create:rotation_speed_controller",
+	"create:hand_crank",
+	"create:spout",
+	"create:steam_engine",
+	"create:deployer",
+	"create:controls",
+	"create:mechanical_drill",
+	"create:extendo_grip",
+	"create:fluid_tank",
 
-  // Create Dreams & Desires
-  Fluid.of("create_dd:condense_milk"),
-  Fluid.of("create_dd:cream"),
-  Fluid.of("create_dd:vanilla"),
-  Fluid.of("create_dd:vanilla_milkshake"),
-  Fluid.of("create_dd:strawberry"),
-  Fluid.of("create_dd:strawberry_milkshake"),
-  Fluid.of("create_dd:glowberry"),
-  Fluid.of("create_dd:glowberry_milkshake"),
-  Fluid.of("create_dd:caramel"),
-  Fluid.of("create_dd:caramel_milkshake"),
-  Fluid.of("create_dd:hot_chocolate"),
-  Fluid.of("create_dd:chocolate_milkshake"),
-  "create_dd:condense_milk_bucket",
-  "create_dd:cream_bucket",
-  "create_dd:vanilla_bucket",
-  "create_dd:vanilla_milkshake_bucket",
-  "create_dd:strawberry_bucket",
-  "create_dd:strawberry_milkshake_bucket",
-  "create_dd:glowberry_bucket",
-  "create_dd:glowberry_milkshake_bucket",
-  "create_dd:caramel_bucket",
-  "create_dd:caramel_milkshake_bucket",
-  "create_dd:hot_chocolate_bucket",
-  "create_dd:chocolate_milkshake_bucket",
+	// Create Dreams & Desires
+	Fluid.of("create_dd:condense_milk"),
+	Fluid.of("create_dd:cream"),
+	Fluid.of("create_dd:vanilla"),
+	Fluid.of("create_dd:vanilla_milkshake"),
+	Fluid.of("create_dd:strawberry"),
+	Fluid.of("create_dd:strawberry_milkshake"),
+	Fluid.of("create_dd:glowberry"),
+	Fluid.of("create_dd:glowberry_milkshake"),
+	Fluid.of("create_dd:caramel"),
+	Fluid.of("create_dd:caramel_milkshake"),
+	Fluid.of("create_dd:hot_chocolate"),
+	Fluid.of("create_dd:chocolate_milkshake"),
+	"create_dd:condense_milk_bucket",
+	"create_dd:cream_bucket",
+	"create_dd:vanilla_bucket",
+	"create_dd:vanilla_milkshake_bucket",
+	"create_dd:strawberry_bucket",
+	"create_dd:strawberry_milkshake_bucket",
+	"create_dd:glowberry_bucket",
+	"create_dd:glowberry_milkshake_bucket",
+	"create_dd:caramel_bucket",
+	"create_dd:caramel_milkshake_bucket",
+	"create_dd:hot_chocolate_bucket",
+	"create_dd:chocolate_milkshake_bucket",
 
-  "create_dd:steel_block",
-  "create_dd:kinetic_motor",
-  "create_dd:accelerator_motor",
-  "create_dd:mithril_ingot",
-  "create_dd:deforester_saw",
-  "create_dd:infernal_mechanism",
-  "create_dd:hydraulic_press",
-  "create_dd:reinforcement_plating",
-  "create_dd:bronze_casing",
-  "create_dd:industrial_iron_ingot",
-  "create_dd:industrial_iron_nugget",
-  "create_dd:industrial_iron_sheet",
-  "create_dd:industrial_iron_block",
-  "createdeco:zinc_sheet",
+	"create_dd:steel_block",
+	"create_dd:kinetic_motor",
+	"create_dd:accelerator_motor",
+	"create_dd:mithril_ingot",
+	"create_dd:deforester_saw",
+	"create_dd:infernal_mechanism",
+	"create_dd:hydraulic_press",
+	"create_dd:reinforcement_plating",
+	"create_dd:bronze_casing",
+	"create_dd:industrial_iron_ingot",
+	"create_dd:industrial_iron_nugget",
+	"create_dd:industrial_iron_sheet",
+	"create_dd:industrial_iron_block",
+	"createdeco:zinc_sheet",
 
-  // Create Things and Misc
-  "create_things_and_misc:radar",
+	// Create Things and Misc
+	"create_things_and_misc:radar",
 
-  // Create Addition
-  "createaddition:digital_adapter",
-  "createaddition:electrum_rod",
-  "createaddition:electrum_sheet",
-  "createaddition:electrum_wire",
-  "createaddition:zinc_sheet",
-  Item.of("createaddition:diamond_grit_sandpaper").weakNBT(),
+	// Create Addition
+	"createaddition:digital_adapter",
+	"createaddition:electrum_rod",
+	"createaddition:electrum_sheet",
+	"createaddition:electrum_wire",
+	"createaddition:zinc_sheet",
+	Item.of("createaddition:diamond_grit_sandpaper").weakNBT(),
 
-  // Create Paper Line
-  "create_paper_line:dryer",
-  "create_paper_line:frame",
+	// Create Paper Line
+	"create_paper_line:dryer",
+	"create_paper_line:frame",
 
-  // Create Industry
-  "createindustry:napalm_bomb",
-  "createindustry:napalm_bucket",
-  "createindustry:thermite_grenade",
-  /createindustry:.*concrete.*/,
-  "createindustry:mesh",
-  "createindustry:thermite_powder",
-  "createindustry:factory_floor",
-  "createindustry:factory_floor_slab",
-  /v_slab_compat:createindustry\/.*factory_floor.*/,
-  "createindustry:industrial_barrel",
-  "createindustry:caution_block",
-  "createindustry:coal_coke",
-  "createindustry:steel_ingot",
-  "createindustry:cast_iron_ingot",
-  "createindustry:cast_iron_block",
-  "createindustry:heavy_plate",
-  "createindustry:steel_scaffolding",
-  "createindustry:sawdust",
+	// Create Industry
+	"createindustry:napalm_bomb",
+	"createindustry:napalm_bucket",
+	"createindustry:thermite_grenade",
+	/createindustry:.*concrete.*/,
+	"createindustry:mesh",
+	"createindustry:thermite_powder",
+	"createindustry:factory_floor",
+	"createindustry:factory_floor_slab",
+	/v_slab_compat:createindustry\/.*factory_floor.*/,
+	"createindustry:industrial_barrel",
+	"createindustry:caution_block",
+	"createindustry:coal_coke",
+	"createindustry:steel_ingot",
+	"createindustry:cast_iron_ingot",
+	"createindustry:cast_iron_block",
+	"createindustry:heavy_plate",
+	"createindustry:steel_scaffolding",
+	"createindustry:sawdust",
 
-  // Every Compat
-  /everycomp:sd\/.*/,
+	// Every Compat
+	/everycomp:sd\/.*/,
 ];
 
 ServerEvents.recipes((event) => {
-  outputsToRemove.forEach((output) => {
-    event.remove({ output: output });
-  });
+	outputsToRemove.forEach((output) => {
+		event.remove({ output: output });
+	});
 });
--- kubejs/server_scripts/create.js
+++ kubejs/server_scripts/create.js
@@ -1,294 +1,251 @@
 ServerEvents.recipes((event) => {
-  const automatedPacking = [
-    { mod: "minecraft", item: "coal", type: "gems" },
-    { mod: "minecraft", item: "gold", type: "ingots" },
-    { mod: "minecraft", item: "iron", type: "ingots" },
-    { mod: "minecraft", item: "copper", type: "ingots" },
-    { mod: "minecraft", item: "redstone", type: "gems" },
-    { mod: "minecraft", item: "diamond", type: "gems" },
-    { mod: "minecraft", item: "netherite", type: "ingots" },
-    { mod: "minecraft", item: "emerald", type: "gems" },
-    { mod: "minecraft", item: "lapis", type: "gems" },
-    { mod: "create_dd", item: "tin", type: "ingots" },
-    { mod: "create", item: "zinc", type: "ingots" },
-    { mod: "create", item: "brass", type: "ingots" },
-    { mod: "create_dd", item: "bronze", type: "ingots" },
-    { mod: "alloyed", item: "steel", type: "ingots" },
-    { mod: "create_dd", item: "mithril", type: "ingots" },
-  ].forEach((block) => {
-    if (block.mod == "create") {
-      event.remove({
-        id: `${block.mod}:crafting/materials/${block.item}_block_from_compacting`,
-      });
-    } else if (block.mod == "create_dd" || block.mod == "alloyed") {
-      event.remove({
-        id: `${block.mod}:crafting/${block.item}_block_from_compacting`,
-      });
-    } else {
-      event.remove({ id: `${block.mod}:${block.item}_block` });
-    }
+	const automatedPacking = [
+		{ mod: "minecraft", item: "coal", type: "gems" },
+		{ mod: "minecraft", item: "gold", type: "ingots" },
+		{ mod: "minecraft", item: "iron", type: "ingots" },
+		{ mod: "minecraft", item: "copper", type: "ingots" },
+		{ mod: "minecraft", item: "redstone", type: "gems" },
+		{ mod: "minecraft", item: "diamond", type: "gems" },
+		{ mod: "minecraft", item: "netherite", type: "ingots" },
+		{ mod: "minecraft", item: "emerald", type: "gems" },
+		{ mod: "minecraft", item: "lapis", type: "gems" },
+		{ mod: "create_dd", item: "tin", type: "ingots" },
+		{ mod: "create", item: "zinc", type: "ingots" },
+		{ mod: "create", item: "brass", type: "ingots" },
+		{ mod: "create_dd", item: "bronze", type: "ingots" },
+		{ mod: "alloyed", item: "steel", type: "ingots" },
+		{ mod: "create_dd", item: "mithril", type: "ingots" },
+	].forEach((block) => {
+		if (block.mod == "create") {
+			event.remove({
+				id: `${block.mod}:crafting/materials/${block.item}_block_from_compacting`,
+			});
+		} else if (block.mod == "create_dd" || block.mod == "alloyed") {
+			event.remove({
+				id: `${block.mod}:crafting/${block.item}_block_from_compacting`,
+			});
+		} else {
+			event.remove({ id: `${block.mod}:${block.item}_block` });
+		}
 
-    event.recipes.create.compacting(
-      `${block.mod}:${block.item}_block`,
-      `9x #forge:${block.type}/${block.item}`
-    );
-  });
+		event.recipes.create.compacting(`${block.mod}:${block.item}_block`, `9x #forge:${block.type}/${block.item}`);
+	});
 
-  // Lapis Alloy
-  event.recipes.create.mixing("create_dd:lapis_alloy", [
-    "minecraft:lapis_lazuli",
-    "#forge:nuggets/tin",
-  ]);
+	// Lapis Alloy
+	event.recipes.create.mixing("create_dd:lapis_alloy", ["minecraft:lapis_lazuli", "#forge:nuggets/tin"]);
 
-  // Raw Iron Washing
-  event.recipes.create.splashing(
-    "9x minecraft:iron_nugget",
-    "create:crushed_raw_iron"
-  );
+	// Raw Iron Washing
+	event.recipes.create.splashing("9x minecraft:iron_nugget", "create:crushed_raw_iron");
 
-  // Diamond Grit Sandpaper
-  event.shapeless(
-    Item.of("createaddition:diamond_grit_sandpaper").withNBT(
-      "{Unbreakable:1b}"
-    ),
-    ["#create:sandpaper", "kubejs:molten_diamond_ingot"]
-  );
+	// Diamond Grit Sandpaper
+	event.shapeless(Item.of("createaddition:diamond_grit_sandpaper").withNBT("{Unbreakable:1b}"), [
+		"#create:sandpaper",
+		"kubejs:molten_diamond_ingot",
+	]);
 
-  // Harder Minecart Assembler
-  event.shaped("create:cart_assembler", ["   ", "TRT", "B B"], {
-    T: "create:railway_casing",
-    R: "create:controls",
-    B: "create:brass_casing",
-  });
+	// Harder Minecart Assembler
+	event.shaped("create:cart_assembler", ["   ", "TRT", "B B"], {
+		T: "create:railway_casing",
+		R: "create:controls",
+		B: "create:brass_casing",
+	});
 
-  // Harder Rotation Speed Controller
-  event.shaped("create:rotation_speed_controller", [" M ", " B ", "   "], {
-    M: "create_dd:integrated_mechanism",
-    B: "create:brass_casing",
-  });
+	// Harder Rotation Speed Controller
+	event.shaped("create:rotation_speed_controller", [" M ", " B ", "   "], {
+		M: "create_dd:integrated_mechanism",
+		B: "create:brass_casing",
+	});
 
-  // Cheap Hand Crank
-  event.shaped(Item.of("create:hand_crank"), ["PPP", "  S", "   "], {
-    P: "#minecraft:planks",
-    S: "minecraft:stick",
-  });
+	// Cheap Hand Crank
+	event.shaped(Item.of("create:hand_crank"), ["PPP", "  S", "   "], {
+		P: "#minecraft:planks",
+		S: "minecraft:stick",
+	});
 
-  // Infernal Mechanism
-  event.recipes
-    .createSequencedAssembly(
-      ["create_dd:infernal_mechanism"],
-      "createdeco:netherite_sheet",
-      [
-        event.recipes.createDeploying(
-          "create_dd:incomplete_infernal_mechanism",
-          ["create_dd:incomplete_infernal_mechanism", "create:cogwheel"]
-        ),
-        event.recipes.createDeploying(
-          "create_dd:incomplete_infernal_mechanism",
-          ["create_dd:incomplete_infernal_mechanism", "minecraft:magma_block"]
-        ),
-        event.recipes.createDeploying(
-          "create_dd:incomplete_infernal_mechanism",
-          ["create_dd:incomplete_infernal_mechanism", "#forge:nuggets/gold"]
-        ),
-        event.recipes.create.filling(
-          "create_dd:incomplete_infernal_mechanism",
-          [
-            Fluid.of("minecraft:lava", 100),
-            "create_dd:incomplete_infernal_mechanism",
-          ]
-        ),
-      ]
-    )
-    .transitionalItem("create_dd:incomplete_infernal_mechanism")
-    .loops(2);
+	// Infernal Mechanism
+	event.recipes
+		.createSequencedAssembly(["create_dd:infernal_mechanism"], "createdeco:netherite_sheet", [
+			event.recipes.createDeploying("create_dd:incomplete_infernal_mechanism", [
+				"create_dd:incomplete_infernal_mechanism",
+				"create:cogwheel",
+			]),
+			event.recipes.createDeploying("create_dd:incomplete_infernal_mechanism", [
+				"create_dd:incomplete_infernal_mechanism",
+				"minecraft:magma_block",
+			]),
+			event.recipes.createDeploying("create_dd:incomplete_infernal_mechanism", [
+				"create_dd:incomplete_infernal_mechanism",
+				"#forge:nuggets/gold",
+			]),
+			event.recipes.create.filling("create_dd:incomplete_infernal_mechanism", [
+				Fluid.of("minecraft:lava", 100),
+				"create_dd:incomplete_infernal_mechanism",
+			]),
+		])
+		.transitionalItem("create_dd:incomplete_infernal_mechanism")
+		.loops(2);
 
-  // Harder Hydraulic Press
-  event.shaped(Item.of("create_dd:hydraulic_press"), [" S ", " H ", " B "], {
-    S: "create_dd:sealed_mechanism",
-    H: "create_dd:hydraulic_casing",
-    B: "create_dd:bronze_casing",
-  });
+	// Harder Hydraulic Press
+	event.shaped(Item.of("create_dd:hydraulic_press"), [" S ", " H ", " B "], {
+		S: "create_dd:sealed_mechanism",
+		H: "create_dd:hydraulic_casing",
+		B: "create_dd:bronze_casing",
+	});
 
-  // Harder Spout
-  event.shaped(Item.of("create:spout"), [" C ", " K ", " S "], {
-    C: "create:copper_casing",
-    K: "minecraft:dried_kelp",
-    S: "create_dd:sealed_mechanism",
-  });
+	// Harder Spout
+	event.shaped(Item.of("create:spout"), [" C ", " K ", " S "], {
+		C: "create:copper_casing",
+		K: "minecraft:dried_kelp",
+		S: "create_dd:sealed_mechanism",
+	});
 
-  // Harder Bronze Casing
-  event.shapeless("create_dd:reinforcement_plating", [
-    "#forge:storage_blocks/steel",
-    "#forge:storage_blocks/bronze",
-    "#forge:storage_blocks/bronze",
-    "#forge:storage_blocks/steel",
-  ]);
-  event.recipes.create.deploying("create_dd:bronze_casing", [
-    "create_dd:reinforcement_plating",
-    "create_dd:bronze_ingot",
-  ]);
+	// Harder Fluid Tank
+	event.shaped("create:fluid_tank", [" C ", " B ", " C "], {
+		C: "#forge:plates/copper",
+		B: "metalbarrels:gold_barrel",
+	});
 
-  // Concrete
-  event.stonecutting(
-    "2x createindustry:concrete_slab",
-    "createindustry:concrete"
-  );
-  event.stonecutting(
-    "createindustry:concrete_stairs",
-    "createindustry:concrete"
-  );
-  event.stonecutting("createindustry:concrete_wall", "createindustry:concrete");
-  event.recipes.create.filling("createindustry:liquid_concrete_bucket", [
-    Fluid.of("createindustry:liquid_concrete_fluid", 1000),
-    "minecraft:bucket",
-  ]);
-  event.recipes.create.emptying(
-    [
-      Fluid.of("createindustry:liquid_concrete_fluid", 1000),
-      "minecraft:bucket",
-    ],
-    "createindustry:liquid_concrete_bucket"
-  );
+	// Harder Bronze Casing
+	event.shapeless("create_dd:reinforcement_plating", [
+		"#forge:storage_blocks/steel",
+		"#forge:storage_blocks/bronze",
+		"#forge:storage_blocks/bronze",
+		"#forge:storage_blocks/steel",
+	]);
+	event.recipes.create.deploying("create_dd:bronze_casing", [
+		"create_dd:reinforcement_plating",
+		"create_dd:bronze_ingot",
+	]);
 
-  // Harder steam engine
-  event.shaped(Item.of("create:steam_engine"), [" G ", "IMS", " C "], {
-    G: "create:golden_sheet",
-    I: "create_dd:infernal_mechanism",
-    M: "create_dd:integrated_mechanism",
-    S: "create_dd:sealed_mechanism",
-    C: "minecraft:copper_block",
-  });
+	// Concrete
+	event.stonecutting("2x createindustry:concrete_slab", "createindustry:concrete");
+	event.stonecutting("createindustry:concrete_stairs", "createindustry:concrete");
+	event.stonecutting("createindustry:concrete_wall", "createindustry:concrete");
+	event.recipes.create.filling("createindustry:liquid_concrete_bucket", [
+		Fluid.of("createindustry:liquid_concrete_fluid", 1000),
+		"minecraft:bucket",
+	]);
+	event.recipes.create.emptying(
+		[Fluid.of("createindustry:liquid_concrete_fluid", 1000), "minecraft:bucket"],
+		"createindustry:liquid_concrete_bucket"
+	);
 
-  // Harder Deployer
-  event.shaped(Item.of("create:deployer"), [" E ", " A ", " B "], {
-    E: "create:electron_tube",
-    A: "create:andesite_casing",
-    B: "create:brass_hand",
-  });
+	// Harder steam engine
+	event.shaped(Item.of("create:steam_engine"), [" G ", "IMS", " C "], {
+		G: "create:golden_sheet",
+		I: "create_dd:infernal_mechanism",
+		M: "create_dd:integrated_mechanism",
+		S: "create_dd:sealed_mechanism",
+		C: "minecraft:copper_block",
+	});
 
-  // Harder Train Controls
-  event.shaped("create:controls", [" L ", " T ", "P I"], {
-    L: "minecraft:lever",
-    T: "create:railway_casing",
-    P: "create:precision_mechanism",
-    I: "create_dd:integrated_mechanism",
-  });
+	// Harder Deployer
+	event.shaped(Item.of("create:deployer"), [" E ", " A ", " B "], {
+		E: "create:electron_tube",
+		A: "create:andesite_casing",
+		B: "create:brass_hand",
+	});
 
-  // Harder Drill
-  event.shaped(Item.of("create:mechanical_drill"), [" A ", "ASA", " C "], {
-    A: "create:andesite_alloy",
-    S: "#forge:plates/steel",
-    C: "create:andesite_casing",
-  });
+	// Harder Train Controls
+	event.shaped("create:controls", [" L ", " T ", "P I"], {
+		L: "minecraft:lever",
+		T: "create:railway_casing",
+		P: "create:precision_mechanism",
+		I: "create_dd:integrated_mechanism",
+	});
 
-  // Shadow Steel
-  event.recipes.create
-    .mixing(
-      [Item.of("create_dd:shadow_steel")],
-      ["minecraft:netherite_block", "8x #forge:plates/steel"]
-    )
-    .superheated();
+	// Harder Drill
+	event.shaped(Item.of("create:mechanical_drill"), [" A ", "ASA", " C "], {
+		A: "create:andesite_alloy",
+		S: "#forge:plates/steel",
+		C: "create:andesite_casing",
+	});
 
-  // Fix brass speaker craft
-  event.shaped(
-    Item.of("create_things_and_misc:brass_speaker"),
-    [" R ", " N ", " R "],
-    {
-      R: "create:railway_casing",
-      N: "minecraft:note_block",
-    }
-  );
+	// Shadow Steel
+	event.recipes.create
+		.mixing([Item.of("create_dd:shadow_steel")], ["minecraft:netherite_block", "8x #forge:plates/steel"])
+		.superheated();
 
-  // Radar
-  event.shaped("create_things_and_misc:radar", ["ASA", "AMA", "AAA"], {
-    A: "create:andesite_alloy",
-    S: "create:display_board",
-    M: "create_dd:integrated_mechanism",
-  });
+	// Fix brass speaker craft
+	event.shaped(Item.of("create_things_and_misc:brass_speaker"), [" R ", " N ", " R "], {
+		R: "create:railway_casing",
+		N: "minecraft:note_block",
+	});
 
-  // Mithril Ingot
-  event.recipes.create
-    .mixing(
-      ["create_dd:mithril_ingot"],
-      [
-        "4x create:experience_nugget",
-        "#forge:ingots/steel",
-        "#forge:ingots/bronze",
-        "create_dd:chromatic_compound",
-        Fluid.of("createindustry:liquid_plastic", 1000),
-      ]
-    )
-    .superheated();
+	// Radar
+	event.shaped("create_things_and_misc:radar", ["ASA", "AMA", "AAA"], {
+		A: "create:andesite_alloy",
+		S: "create:display_board",
+		M: "create_dd:integrated_mechanism",
+	});
 
-  // Asphalt
-  event.recipes.create
-    .mixing(
-      ["16x create_dd:asphalt_block"],
-      ["2x create:scoria", Fluid.of("create_things_and_misc:slime", 100)]
-    )
-    .heated();
+	// Mithril Ingot
+	event.recipes.create
+		.mixing(
+			["create_dd:mithril_ingot"],
+			[
+				"4x create:experience_nugget",
+				"#forge:ingots/steel",
+				"#forge:ingots/bronze",
+				"create_dd:chromatic_compound",
+				Fluid.of("createindustry:liquid_plastic", 1000),
+			]
+		)
+		.superheated();
 
-  // Electron Tube
-  event.shaped("8x create:electron_tube", [" R ", " N ", " P "], {
-    R: "create:polished_rose_quartz",
-    N: "#forge:nuggets/copper",
-    P: "createindustry:plastic_sheet",
-  });
+	// Asphalt
+	event.recipes.create
+		.mixing(["16x create_dd:asphalt_block"], ["2x create:scoria", Fluid.of("create_things_and_misc:slime", 100)])
+		.heated();
 
-  // Fix Dough
-  event.replaceInput({ input: "create:dough" }, "create:dough", "#forge:dough");
-  event.replaceInput(
-    { input: "farmersdelight:wheat_dough" },
-    "farmersdelight:wheat_dough",
-    "#forge:dough"
-  );
+	// Electron Tube
+	event.shaped("8x create:electron_tube", [" R ", " N ", " P "], {
+		R: "create:polished_rose_quartz",
+		N: "#forge:nuggets/copper",
+		P: "createindustry:plastic_sheet",
+	});
 
-  // Nerf Tuff Crusing
-  event.recipes.create.crushing(
-    ["create_dd:potassic_cobble"],
-    "#create:stone_types/tuff"
-  );
+	// Fix Dough
+	event.replaceInput({ input: "create:dough" }, "create:dough", "#forge:dough");
+	event.replaceInput({ input: "farmersdelight:wheat_dough" }, "farmersdelight:wheat_dough", "#forge:dough");
 
-  // Remove Electrum
-  event.replaceOutput(
-    { output: "createaddition:electrum_nugget" },
-    "#forge:nuggets/electrum",
-    Item.of("minecraft:air")
-  );
+	// Nerf Tuff Crusing
+	event.recipes.create.crushing(["create_dd:potassic_cobble"], "#create:stone_types/tuff");
 
-  // Fix Coal Coke
-  event.recipes.create
-    .mixing("createindustry:coal_coke", "#minecraft:coals")
-    .heated();
+	// Remove Electrum
+	event.replaceOutput(
+		{ output: "createaddition:electrum_nugget" },
+		"#forge:nuggets/electrum",
+		Item.of("minecraft:air")
+	);
 
-  // Harder Steel
-  event.recipes.create
-    .compacting("create_dd:steel_ingot", [
-      "2x #forge:ingots/cast_iron",
-      "createindustry:coal_coke",
-    ])
-    .heated()
-    .id("kubejs:steel_ingot");
+	// Fix Coal Coke
+	event.recipes.create.mixing("createindustry:coal_coke", "#minecraft:coals").heated();
 
-  // Harder Sails
-  const sailWoods = [
-    "oak",
-    "spruce",
-    "birch",
-    "jungle",
-    "acacia",
-    "dark_oak",
-    "crimson",
-    "warped",
-    "mangrove",
-    "bamboo",
-  ];
-  sailWoods.forEach((wood) => {
-    event.remove({ output: `create_things_and_misc:${wood}_sail` });
-    event.shapeless(`create_things_and_misc:${wood}_sail`, [
-      "#kubejs:windmill_sails",
-      wood == "bamboo" ? `quark:bamboo_planks` : `minecraft:${wood}_planks`,
-    ]);
-  });
+	// Harder Steel
+	event.recipes.create
+		.compacting("create_dd:steel_ingot", ["2x #forge:ingots/cast_iron", "createindustry:coal_coke"])
+		.heated()
+		.id("kubejs:steel_ingot");
+
+	// Harder Sails
+	const sailWoods = [
+		"oak",
+		"spruce",
+		"birch",
+		"jungle",
+		"acacia",
+		"dark_oak",
+		"crimson",
+		"warped",
+		"mangrove",
+		"bamboo",
+	];
+	sailWoods.forEach((wood) => {
+		event.remove({ output: `create_things_and_misc:${wood}_sail` });
+		event.shapeless(`create_things_and_misc:${wood}_sail`, [
+			"#kubejs:windmill_sails",
+			wood == "bamboo" ? `quark:bamboo_planks` : `minecraft:${wood}_planks`,
+		]);
+	});
 });
