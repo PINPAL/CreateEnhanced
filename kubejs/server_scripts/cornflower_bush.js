aee8135c222f80d9556e5d19e5e2f267248b4d47
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-11-02 07:01:30 +0000

    Support block via wrench (jank)

--- kubejs/server_scripts/cornflower_bush.js
+++ kubejs/server_scripts/cornflower_bush.js
@@ -1,84 +1,72 @@
 // Placement
 BlockEvents.rightClicked((event) => {
-  // If the block clicked on is farmland,
-  // the face clicked on is the top
-  // and the item is a cornflower bush
-  if (
-    event.getBlock().hasTag("minecraft:azalea_grows_on") &&
-    event.getFacing() == "up" &&
-    event.getItem().getId() == "kubejs:cornflower_bush_item"
-  ) {
-    // Get the block above the block clicked on
-    let blockAbove = event.getBlock().getUp();
-    // Get the block above the block above the block clicked on
-    let blockAbove2 = blockAbove.getUp();
-    // Check if the block above and the block above the block above are air
-    if (
-      blockAbove.getId() == "minecraft:air" &&
-      blockAbove2.getId() == "minecraft:air"
-    ) {
-      // Set the block above to a cornflower bush (bottom half)
-      blockAbove.set("kubejs:cornflower_bush", { half: "bottom" });
-      // Set the block above the block above to a cornflower bush (top half)
-      blockAbove2.set("kubejs:cornflower_bush", { half: "top" });
-      // Play a sound
-      event.server.runCommandSilent(
-        `playsound minecraft:block.grass.place block @a ${blockAbove.getX()} ${blockAbove.getY()} ${blockAbove.getZ()}`
-      );
-    }
-  }
+	// If the block clicked on is farmland,
+	// the face clicked on is the top
+	// and the item is a cornflower bush
+	if (
+		event.getBlock().hasTag("minecraft:azalea_grows_on") &&
+		event.getFacing() == "up" &&
+		event.getItem().getId() == "kubejs:cornflower_bush_item"
+	) {
+		// Get the block above the block clicked on
+		let blockAbove = event.getBlock().getUp();
+		// Get the block above the block above the block clicked on
+		let blockAbove2 = blockAbove.getUp();
+		// Check if the block above and the block above the block above are air
+		if (blockAbove.getId() == "minecraft:air" && blockAbove2.getId() == "minecraft:air") {
+			// Set the block above to a cornflower bush (bottom half)
+			blockAbove.set("kubejs:cornflower_bush", { half: "bottom" });
+			// Set the block above the block above to a cornflower bush (top half)
+			blockAbove2.set("kubejs:cornflower_bush", { half: "top" });
+			// Play a sound
+			event.server.runCommandSilent(
+				`playsound minecraft:block.grass.place block @a ${blockAbove.getX()} ${blockAbove.getY()} ${blockAbove.getZ()}`
+			);
+		}
+	}
 });
 
 function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
 }
 
 // Bonemeal
 BlockEvents.rightClicked((event) => {
-  let block = event.getBlock();
-  if (
-    block.getId() == "kubejs:cornflower_bush" &&
-    event.getItem().getId() == "minecraft:bone_meal"
-  ) {
-    event.server.runCommandSilent(
-      `particle minecraft:composter ${block.getX()} ${block.getY()} ${block.getZ()} .25 .25 .25 1 20 normal`
-    );
-    event.server.runCommandSilent(
-      `summon item ${block.getX()} ${block.getY()} ${block.getZ()} 
-      {Motion:[${getRandomArbitrary(-0.1, 0.1)}, ${getRandomArbitrary(
-        0.1,
-        0.3
-      )}, ${getRandomArbitrary(
-        -0.1,
-        0.1
-      )}],Item:{id:"kubejs:cornflower_bush_item",Count:1b}}`
-    );
-    event.player.setHeldItem(
-      event.getHand(),
-      `${event.getItem().getCount() - 1}x minecraft:bone_meal`
-    );
-  }
+	let block = event.getBlock();
+	if (block.getId() == "kubejs:cornflower_bush" && event.getItem().getId() == "minecraft:bone_meal") {
+		event.server.runCommandSilent(
+			`particle minecraft:composter ${block.getX()} ${block.getY()} ${block.getZ()} .25 .25 .25 1 20 normal`
+		);
+		event.server.runCommandSilent(
+			`summon item ${block.getX()} ${block.getY()} ${block.getZ()} 
+      {Motion:[${getRandomArbitrary(-0.1, 0.1)}, ${getRandomArbitrary(0.1, 0.3)}, ${getRandomArbitrary(
+				-0.1,
+				0.1
+			)}],Item:{id:"kubejs:cornflower_bush_item",Count:1b}}`
+		);
+		event.player.setHeldItem(event.getHand(), `${event.getItem().getCount() - 1}x minecraft:bone_meal`);
+	}
 });
 
 // Breaking
 BlockEvents.broken((event) => {
-  let half = event.getBlock().getBlockState().getValue(BlockProperties.HALF);
-  if (half == "top") {
-    let blockBelow = event.getBlock().getDown();
-    if (blockBelow.getId() == "kubejs:cornflower_bush") {
-      blockBelow.set("minecraft:air");
-    }
-  } else if (half == "bottom") {
-    let blockAbove = event.getBlock().getUp();
-    if (blockAbove.getId() == "kubejs:cornflower_bush") {
-      blockAbove.set("minecraft:air");
-    }
-  }
+	if (event.getBlock().getId() == "kubejs:cornflower_bush") {
+		let half = event.getBlock().getBlockState().getValue(BlockProperties.HALF);
+		if (half == "top") {
+			let blockBelow = event.getBlock().getDown();
+			if (blockBelow.getId() == "kubejs:cornflower_bush") {
+				blockBelow.set("minecraft:air");
+			}
+		} else if (half == "bottom") {
+			let blockAbove = event.getBlock().getUp();
+			if (blockAbove.getId() == "kubejs:cornflower_bush") {
+				blockAbove.set("minecraft:air");
+			}
+		}
+	}
 });
 
 // Recipe
 ServerEvents.recipes((event) => {
-  event.recipes.create.compacting("kubejs:cornflower_bush_item", [
-    "9x minecraft:cornflower",
-  ]);
+	event.recipes.create.compacting("kubejs:cornflower_bush_item", ["9x minecraft:cornflower"]);
 });
--- kubejs/server_scripts/decorative_blocks.js
+++ kubejs/server_scripts/decorative_blocks.js
@@ -1,26 +1,72 @@
-// const ModBlockProperties = Java.loadClass(
-//   "lilypuree.decorative_blocks.blocks.state.ModBlockProperties"
-// );
-// const SupportFaceShape = Java.loadClass(
-//   "lilypuree.decorative_blocks.blocks.state.SupportFaceShape"
-// );
+const ModBlockProperties = Java.loadClass("lilypuree.decorative_blocks.blocks.state.ModBlockProperties");
+const SupportFaceShape = Java.loadClass("lilypuree.decorative_blocks.blocks.state.SupportFaceShape");
 
 ServerEvents.recipes((event) => {
-  // Lattice
-  event.shaped(Item.of("decorative_blocks:lattice"), ["SS ", "SS ", "   "], {
-    S: "minecraft:stick",
-  });
+	// Lattice
+	event.shaped(Item.of("decorative_blocks:lattice"), ["SS ", "SS ", "   "], {
+		S: "minecraft:stick",
+	});
 });
 
-// BlockEvents.rightClicked((event) => {
-//   if (
-//     event.getBlock().hasTag("decorative_blocks:supports") &&
-//     event.getItem() == "create:wrench"
-//   ) {
-//     let blockState = event.getBlock().getBlockState();
+BlockEvents.rightClicked((event) => {
+	let block = event.getBlock();
+	let isCrouching = event.getPlayer().isCrouching();
+	let sideClickedOn = event.getFacing();
+	if (block.hasTag("decorative_blocks:supports") && event.getItem() == "create:wrench") {
+		let blockState = event.getBlock().getBlockState();
 
-//     console.log(blockState);
-//     console.log(blockState.getValue(BlockProperties.UP));
-//     blockState.setValue(BlockProperties.UP, false);
-//   }
-// });
+		// let facing = blockState.getValue(BlockProperties.FACING);
+		let waterlogged = blockState.getValue(BlockProperties.WATERLOGGED);
+		let up = blockState.getValue(BlockProperties.UP);
+		let horizontal = blockState.getValue(ModBlockProperties.HORIZONTAL_SHAPE);
+		let vertical = blockState.getValue(ModBlockProperties.VERTICAL_SHAPE);
+
+		// console.log(facing);
+		// console.log(up);
+		// console.log(horizontal);
+		// console.log(vertical);
+		// console.log(sideClickedOn);
+		if (sideClickedOn == Direction.UP) {
+			// If crouching toggle hiding the side
+			if (isCrouching) {
+				block.set(block.getId(), {
+					up: up,
+					horizontal:
+						horizontal == SupportFaceShape.HIDDEN ? SupportFaceShape.SMALL : SupportFaceShape.HIDDEN,
+					vertical: vertical,
+					waterlogged: waterlogged,
+				});
+			} else if (!isCrouching) {
+				// Ensure that the side is not hidden
+				if (horizontal != SupportFaceShape.HIDDEN) {
+					block.set(block.getId(), {
+						up: up,
+						horizontal: horizontal == SupportFaceShape.BIG ? SupportFaceShape.SMALL : SupportFaceShape.BIG,
+						vertical: vertical,
+						waterlogged: waterlogged,
+					});
+				}
+			}
+		} else {
+			// If crouching toggle hiding the side
+			if (isCrouching) {
+				block.set(block.getId(), {
+					up: up,
+					horizontal: horizontal,
+					vertical: vertical == SupportFaceShape.HIDDEN ? SupportFaceShape.SMALL : SupportFaceShape.HIDDEN,
+					waterlogged: waterlogged,
+				});
+			} else if (!isCrouching) {
+				// Ensure that the side is not hidden
+				if (vertical != SupportFaceShape.HIDDEN) {
+					block.set(block.getId(), {
+						up: up,
+						horizontal: horizontal,
+						vertical: vertical == SupportFaceShape.BIG ? SupportFaceShape.SMALL : SupportFaceShape.BIG,
+						waterlogged: waterlogged,
+					});
+				}
+			}
+		}
+	}
+});
