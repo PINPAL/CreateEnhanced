e7187cabe289e79beda0bc616cc2a7e81663f05d
PINPAL <18197217+PINPAL@users.noreply.github.com>
2023-11-02 04:42:26 +0000

    Metal barrels recipes ingot -> plates

--- kubejs/server_scripts/metal_barrels.js
+++ kubejs/server_scripts/metal_barrels.js
@@ -1,28 +1,10 @@
 ServerEvents.recipes((event) => {
-  event.recipes.create.deploying("metalbarrels:iron_barrel", [
-    "minecraft:barrel",
-    "create:andesite_alloy",
-  ]);
-  event.recipes.create.deploying("metalbarrels:wood_to_iron", [
-    "minecraft:stick",
-    "create:andesite_alloy",
-  ]);
+	event.recipes.create.deploying("metalbarrels:iron_barrel", ["minecraft:barrel", "#forge:plates/andesite_alloy"]);
+	event.recipes.create.deploying("metalbarrels:wood_to_iron", ["minecraft:stick", "#forge:plates/andesite_alloy"]);
 
-  event.recipes.create.deploying("metalbarrels:gold_barrel", [
-    "metalbarrels:iron_barrel",
-    "#forge:ingots/copper",
-  ]);
-  event.recipes.create.deploying("metalbarrels:iron_to_gold", [
-    "minecraft:stick",
-    "#forge:ingots/copper",
-  ]);
+	event.recipes.create.deploying("metalbarrels:gold_barrel", ["metalbarrels:iron_barrel", "#forge:plates/copper"]);
+	event.recipes.create.deploying("metalbarrels:iron_to_gold", ["minecraft:stick", "#forge:plates/copper"]);
 
-  event.recipes.create.deploying("metalbarrels:diamond_barrel", [
-    "metalbarrels:gold_barrel",
-    "#forge:ingots/brass",
-  ]);
-  event.recipes.create.deploying("metalbarrels:gold_to_diamond", [
-    "minecraft:stick",
-    "#forge:ingots/brass",
-  ]);
+	event.recipes.create.deploying("metalbarrels:diamond_barrel", ["metalbarrels:gold_barrel", "#forge:plates/brass"]);
+	event.recipes.create.deploying("metalbarrels:gold_to_diamond", ["minecraft:stick", "#forge:plates/brass"]);
 });
