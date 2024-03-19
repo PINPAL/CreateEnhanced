8bbabe1dfb2dec61a4bd457146fb03f10f599677
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-03-19 12:10:14 +0000

    Vertical Slab Fix

--- kubejs/server_scripts/vertical_slabs.js
+++ kubejs/server_scripts/vertical_slabs.js
@@ -1,17 +1,6 @@
 var vertical_slabs = [];
 
-const supplementariesVerticalSlabs = ["checker", "ash_bricks", "stone_tile", "blackstone_tile"];
-const createDecoSlabs = [
-	"andesite_sheet",
-	"brass_sheet",
-	"cast_iron_sheet",
-	"copper_sheet",
-	"gold_sheet",
-	"iron_sheet",
-	"netherite_sheet",
-	"zinc_sheet",
-];
-
+// Fetch all vertical slabs from v_slab_compat
 ServerEvents.tags("item", (event) => {
 	let v_slabs = event.get("v_slab_compat:vertical_slabs").getObjectIds();
 	v_slabs.forEach((slab) => {
@@ -32,34 +21,11 @@ ServerEvents.recipes((event) => {
 			event.shapeless(outputSlab, inputSlab[0].getItemIds()[0].toString());
 		});
 
-	// Handle Create Deco Slabs
-	event
-		.findRecipes({
-			type: "minecraft:crafting_shaped",
-			output: /createdeco:.*sheet_slab/,
-		})
-		.forEach((recipe) => {
-			let outputSlab = "2x " + recipe.getOriginalRecipeResult().getId() + "_vert";
-			let inputBlock = recipe.getOriginalRecipeIngredients();
-			event.stonecutting(outputSlab, inputBlock[0].getItemIds()[0].toString());
-		});
-	createDecoSlabs.forEach((slab) => {
-		event.shapeless(`createdeco:${slab}_slab_vert`, `createdeco:${slab}_slab`);
-		event.shaped(`3x createdeco:${slab}_slab_vert`, [" S ", " S ", " S "], {
-			S: `createdeco:${slab}_slab`,
-		});
-	});
-
 	// Handle v_slab_compat
 	vertical_slabs.forEach((item) => {
 		event.shapeless(
 			item,
 			item.getId().replace("v_slab_compat:", "").replace("_vertical_slab", "_slab").replace("/", ":")
 		);
-	});
-
-	// Handle Supplementaries
-	supplementariesVerticalSlabs.forEach((slab) => {
-		event.shapeless(`supplementaries:${slab}_vertical_slab`, `supplementaries:${slab}_slab`);
 	});
 });
