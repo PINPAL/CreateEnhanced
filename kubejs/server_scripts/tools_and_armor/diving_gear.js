2b75416961b208ee494ac29f33cf8a1b4f49e963
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-03-19 08:34:24 +0000

    Make Diving Gear require copper gearset

--- kubejs/server_scripts/tools_and_armor/diving_gear.js
+++ kubejs/server_scripts/tools_and_armor/diving_gear.js
@@ -1,11 +1,20 @@
 ServerEvents.recipes((event) => {
-  event.shaped("create:copper_diving_helmet", ["CCC", "CGC", "   "], {
-    C: "#forge:ingots/copper",
-    G: "#forge:glass",
-  });
+	event.shaped("create:copper_diving_helmet", ["CCC", "CGC", " B "], {
+		C: "#forge:ingots/copper",
+		G: "#forge:glass",
+		B: "kubejs:copper_helmet",
+	});
 
-  event.shaped("create:copper_diving_boots", ["C C", "C C", "A A"], {
-    C: "#forge:ingots/copper",
-    A: "create:andesite_alloy",
-  });
+	event.shaped("create:copper_diving_boots", ["CBC", "C C", "A A"], {
+		C: "#forge:ingots/copper",
+		A: "create:andesite_alloy",
+		B: "kubejs:copper_boots",
+	});
+
+	event.shaped("create:copper_backtank", ["ASA", "CBC", " C "], {
+		A: "create:andesite_alloy",
+		S: "create:shaft",
+		C: "#forge:ingots/copper",
+		B: "kubejs:copper_chestplate",
+	});
 });
