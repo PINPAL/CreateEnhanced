dd671e0fa4df974f0f115bbd6999ff5f26eb062e
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-03-20 09:34:41 +0000

    Nourishment Hunches

--- kubejs/server_scripts/nutrionalbalance.js
+++ kubejs/server_scripts/nutrionalbalance.js
@@ -9,36 +9,133 @@ ServerEvents.recipes((event) => {
 
 const nutritionAPI = Java.loadClass("com.dannyandson.nutritionalbalance.nutrients.PlayerNutritionData");
 
-function generateHunchString(nutrient, fillLevel) {
-	let hunchOutline = "";
-	let hunchFilled = "";
-	if (nutrient === "veggies") {
-		hunchOutline = "\uEff1";
-		hunchFilled = "\uEff2";
-	}
-	if (nutrient === "proteins") {
-		hunchOutline = "\uEff3";
-		hunchFilled = "\uEff4";
-	}
-	if (nutrient === "carbs") {
-		hunchOutline = "\uEff5";
-		hunchFilled = "\uEff6";
-	}
+const nutrientHunchs = {
+	veggies: {
+		outline: "\uEff5",
+		filled: "\uEff6",
+		half: "\uEff7",
+	},
+	proteins: {
+		outline: "\uEff3",
+		filled: "\uEff4",
+		half: "\uEff8",
+	},
+	carbs: {
+		outline: "\uEff1",
+		filled: "\uEff2",
+		half: "\uEff9",
+	},
+};
 
-	if (fillLevel <= 10) {
-		return hunchOutline + hunchOutline + hunchOutline;
-	}
-	if (fillLevel <= 33) {
-		return hunchFilled + hunchOutline + hunchOutline;
-	}
-	if (fillLevel <= 66) {
-		return hunchFilled + hunchFilled + hunchOutline;
-	} else {
-		return hunchFilled + hunchFilled + hunchFilled;
-	}
+function roundToNearestHalf(number) {
+	return Math.round(number * 2) / 2;
 }
 
+PlayerEvents.loggedIn((event) => {
+	// Draw Hunches (empty)
+	event.player.paint({
+		nutrientHunchs: {
+			proteinOne: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 15,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			proteinTwo: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 23,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			proteinThree: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 31,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			carbsOne: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 43,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			carbsTwo: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 51,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			carbsThree: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 59,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			veggiesOne: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 71,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			veggiesTwo: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 79,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+			veggiesThree: {
+				type: "text",
+				text: "",
+				draw: "ingame",
+				scale: 1.0,
+				x: 87,
+				y: -37,
+				alignX: "center",
+				alignY: "bottom",
+			},
+		},
+	});
+});
+
+var hunchYPos = -37;
+var hunchSkakePos = 1;
+
 PlayerEvents.tick((event) => {
+	if (event.player.isCreative()) return;
+	if (event.player.isSpectator()) return;
+	// Fetch Nutrients
 	let nutrients = nutritionAPI.getWorldNutritionData().getNutritionalBalancePlayer(event.player).getPlayerNutrients();
 	let nutrientVeggies = "";
 	let nutrientProteins = "";
@@ -46,53 +143,124 @@ PlayerEvents.tick((event) => {
 	nutrients.forEach((nutrient) => {
 		let nutrientName = nutrient.getNutrientName();
 		let nutrientValue = nutrient.getValue();
+		// Update Variables to store the nutrient values
+		// as a number between 0 and 3, representing the number of hunches
+		// if the nutrient value is less than 10 (malnourished), it will be considered as 0
 		switch (nutrientName) {
 			case "vegetables":
-				nutrientVeggies = nutrientValue.toFixed(1);
+				nutrientVeggies = nutrientValue < 10 ? 0 : roundToNearestHalf(nutrientValue / 33.333);
 				break;
 			case "proteins":
-				nutrientProteins = nutrientValue.toFixed(1);
+				nutrientProteins = nutrientValue < 10 ? 0 : roundToNearestHalf(nutrientValue / 33.333);
 				break;
 			case "carbs":
-				nutrientCarbs = nutrientValue.toFixed(1);
+				nutrientCarbs = nutrientValue < 10 ? 0 : roundToNearestHalf(nutrientValue / 33.333);
 				break;
 		}
 	});
+	// Update Hunches
 	event.player.paint({
-		veggies: { remove: true },
-		proteins: { remove: true },
-		carbs: { remove: true },
+		proteinOne: {
+			text:
+				nutrientProteins >= 1
+					? nutrientHunchs.proteins.filled
+					: nutrientProteins >= 0.5
+					? nutrientHunchs.proteins.half
+					: nutrientHunchs.proteins.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 1 ? (nutrientProteins < 0.5 ? hunchYPos : -37) : -37,
+		},
+		proteinTwo: {
+			text:
+				nutrientProteins >= 2
+					? nutrientHunchs.proteins.filled
+					: nutrientProteins >= 1.5
+					? nutrientHunchs.proteins.half
+					: nutrientHunchs.proteins.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 2 ? (nutrientProteins < 0.5 ? hunchYPos : -37) : -37,
+		},
+		proteinThree: {
+			text:
+				nutrientProteins >= 3
+					? nutrientHunchs.proteins.filled
+					: nutrientProteins >= 2.5
+					? nutrientHunchs.proteins.half
+					: nutrientHunchs.proteins.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 3 ? (nutrientProteins < 0.5 ? hunchYPos : -37) : -37,
+		},
+		carbsOne: {
+			text:
+				nutrientCarbs >= 1
+					? nutrientHunchs.carbs.filled
+					: nutrientCarbs >= 0.5
+					? nutrientHunchs.carbs.half
+					: nutrientHunchs.carbs.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 1 ? (nutrientCarbs < 0.5 ? hunchYPos : -37) : -37,
+		},
+		carbsTwo: {
+			text:
+				nutrientCarbs >= 2
+					? nutrientHunchs.carbs.filled
+					: nutrientCarbs >= 1.5
+					? nutrientHunchs.carbs.half
+					: nutrientHunchs.carbs.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 2 ? (nutrientCarbs < 0.5 ? hunchYPos : -37) : -37,
+		},
+		carbsThree: {
+			text:
+				nutrientCarbs >= 3
+					? nutrientHunchs.carbs.filled
+					: nutrientCarbs >= 2.5
+					? nutrientHunchs.carbs.half
+					: nutrientHunchs.carbs.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 3 ? (nutrientCarbs < 0.5 ? hunchYPos : -37) : -37,
+		},
+		veggiesOne: {
+			text:
+				nutrientVeggies >= 1
+					? nutrientHunchs.veggies.filled
+					: nutrientVeggies >= 0.5
+					? nutrientHunchs.veggies.half
+					: nutrientHunchs.veggies.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 1 ? (nutrientVeggies < 0.5 ? hunchYPos : -37) : -37,
+		},
+		veggiesTwo: {
+			text:
+				nutrientVeggies >= 2
+					? nutrientHunchs.veggies.filled
+					: nutrientVeggies >= 1.5
+					? nutrientHunchs.veggies.half
+					: nutrientHunchs.veggies.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 2 ? (nutrientVeggies < 0.5 ? hunchYPos : -37) : -37,
+		},
+		veggiesThree: {
+			text:
+				nutrientVeggies >= 3
+					? nutrientHunchs.veggies.filled
+					: nutrientVeggies >= 2.5
+					? nutrientHunchs.veggies.half
+					: nutrientHunchs.veggies.outline,
+			// Shake hunch if the player is malnourished
+			y: hunchSkakePos == 3 ? (nutrientVeggies < 0.5 ? hunchYPos : -37) : -37,
+		},
 	});
-	event.player.paint({
-		veggies: {
-			type: "text",
-			text: generateHunchString("veggies", nutrientVeggies),
-			draw: "ingame",
-			scale: 1.0,
-			x: 25,
-			y: -37,
-			alignX: "center",
-			alignY: "bottom",
-		},
-		proteins: {
-			type: "text",
-			text: generateHunchString("proteins", nutrientProteins),
-			draw: "ingame",
-			scale: 1.0,
-			x: 55,
-			y: -37,
-			alignX: "center",
-			alignY: "bottom",
-		},
-		carbs: {
-			type: "text",
-			text: generateHunchString("carbs", nutrientCarbs),
-			draw: "ingame",
-			scale: 1.0,
-			x: 85,
-			y: -37,
-			alignX: "center",
-			alignY: "bottom",
-		},
-	});
+
+	// Update Hunch Y Position
+	if (hunchYPos == -37) {
+		hunchYPos = -38;
+	} else {
+		hunchYPos = -37;
+	}
+	if (hunchSkakePos == 3) {
+		hunchSkakePos = 1;
+	} else {
+		hunchSkakePos++;
+	}
 });
